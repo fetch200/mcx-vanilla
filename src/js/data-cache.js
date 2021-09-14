8e1c8bd34f6df1d9f9843e89bd68d52a9c844509
machaoxue <machaoxue@yeah.net>
2021-09-15 00:29:40 +0800

    v0.0.2

---
+++ src/js/data-cache.js
@@ -0,0 +1,11 @@
+// 数据缓存
+
+const dataCache = {
+  // 数据缓存
+  getStorage(){},
+  setStorage(){},
+  removeStorage(){},
+  clearStorage(){}
+}
+
+export {dataCache}
---
+++ src/js/device.js
@@ -0,0 +1,11 @@
+// 设备
+
+const device = {
+  // 系统信息
+  getSystemInfo(){},
+  // 剪切板
+  setClipboardData(){},
+  getClipboardData(){},
+}
+
+export {device}
